23eeec054c7c194a59b0d1e7b3295860925d7ec2
hippytaff <hippytaff@gmail.com>
2016-01-26 11:09:47 -0600

    Update description

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description
 # - Builds drat environment from bodhilinux 3.1.1
-#
+# - run from $HOME
 # Todo
 # - build script to build from ubuntu minimal (Whichever stable is about by the time I get around to it)
 
