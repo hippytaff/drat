a55bfaaf63227d8a33a632f9a0297f71665d9f3b
hippytaff <hippytaff@gmail.com>
2016-02-04 17:26:52 +0000

    change config paths and check and install missing deps

--- build.sh
+++ build.sh
@@ -11,15 +11,15 @@ sudo echo "## Bodhi Linux Repository - for E and Terminology for now..." >> /etc
 sudo echo "deb http://packages.bodhilinux.com/bodhi trusty main" >> /etc/apt/sources.list
 
 # Get deps
-sudo apt-get update && sudo apt-get install -y xorg ubiquity ratpoison xinit xterm xcompmgr terminology mc scrot irssi tty-clock nsnake firefox tree flashplugin-installer plymouth-label plymouth-x11 plymouth-theme-script
+sudo apt-get update && sudo apt-get install -y xorg ubiquity ratpoison xinit xterm xcompmgr terminology mc scrot irssi tty-clock nsnake firefox tree flashplugin-installer plymouth-label plymouth-x11 plymouth-theme-script && sudo apt-get -f install
 
 # Clean-up
 sudo apt-get autoremove -y
 
-# Copy configs
+# Copy configs - to do: put files in relelvant /etc/pkgrc
 sudo cp ~/drat/configs/.* ~/
 sudo cp ~/drat/configs/other/.gitconfig ~/ # My gitconfig
 mkdir .drat
-sudo cp ~/drat/drat-logo.png .drat/
+sudo cp ~/drat/drat-logo.png ~/usr/share/terminology/backgrounds
 # start xserver
 startx
