5a661e48d42d7f90bcd27ec75d079667a79a8e14
hippytaff <hippytaff@gmail.com>
2016-02-07 03:40:49 -0600

    fix old build.sh

--- old/build.sh
+++ old/build.sh
@@ -12,8 +12,9 @@ sudo apt-get update && sudo apt-get install -y ratpoison xinit xterm xcompmgr te
 sudo apt-get autoremove -y
 
 # Copy configs
-sudo cp /drat/old/.rapoisonrc  .
-sudo cp /drat/configs/other/.gitconfig / # My gitconfig
+cp /drat/configs/rapoisonrc .
+mv ratpoisonrc .ratpoisonrc
+sudo cp /drat/configs/other/.gitconfig . # My gitconfig
 sudo cp /drat/configs/.xinit .
 mkdir .drat
 sudo cp /drat/drat-logo.png .drat/
