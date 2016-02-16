c1603207dfe81cc4a87ac65aa25314fa70cf641e
hippytaff <hippytaff@gmail.com>
2016-02-16 19:20:53 +0000

    update buildscripts

--- OpenBSD/build.sh
+++ OpenBSD/build.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 # Description
-# - Builds drat environment Ubuntu minimal
+# - Builds drat environment from OpenBSD (latest stable)
 # - run from $HOME
 
 # TODO
 # - Tidy up and fool proof (only if used by not me, even though I'm a fool)
-
-# Add Bodhilinux repo for E and Terminology that Jeff looks after
-sudo cp /etc/apt/sources.list /etc/apt/~sources.list
-sudo echo "## Bodhi Linux Repository - for E and Terminology for now..." >> /etc/apt/sources.list
-sudo echo "deb http://packages.bodhilinux.com/bodhi trusty main" >> /etc/apt/sources.list
+# - TBH not really sure
 
 # Get deps
-sudo apt-get update && sudo apt-get install -y xorg ubiquity ratpoison xinit xterm xcompmgr terminology mc scrot irssi tty-clock nsnake firefox tree flashplugin-installer plymouth-label plymouth-x11 plymouth-theme-script && sudo apt-get -f install
 
 # Clean-up
-sudo apt-get autoremove -y
 
 # Copy configs
 sudo cp ~/drat/configs/ratpoisonrc /etc/ratpoisonrc
 sudo cp ~/drat/configs/other/.gitconfig ~/ # My gitconfig
-mkdir .drat
-sudo cp ~/drat/drat-logo.png ~/usr/share/terminology/backgrounds
+
 # start xserver
 startx
--- ubuntu-minimal/build.sh
+++ ubuntu-minimal/build.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 # Description
-# - Builds drat environment Ubuntu minimal
+# - Builds drat environment from ubuntu minimal.iso (Stable)
 # - run from $HOME
 
 # TODO
 # - Tidy up and fool proof (only if used by not me, even though I'm a fool)
-
-# Add Bodhilinux repo for E and Terminology that Jeff looks after
-sudo cp /etc/apt/sources.list /etc/apt/~sources.list
-sudo echo "## Bodhi Linux Repository - for E and Terminology for now..." >> /etc/apt/sources.list
-sudo echo "deb http://packages.bodhilinux.com/bodhi trusty main" >> /etc/apt/sources.list
+# - check deps availability in ubuntu repos.
+# - build from source if not available, or warn user that they must do this before proceeding?
 
 # Get deps
-sudo apt-get update && sudo apt-get install -y xorg ubiquity ratpoison xinit xterm xcompmgr terminology mc scrot irssi tty-clock nsnake firefox tree flashplugin-installer plymouth-label plymouth-x11 plymouth-theme-script && sudo apt-get -f install
+sudo apt-get update && sudo apt-get install -y xorg ratpoison xinit xterm xcompmgr terminology mc scrot irssi tty-clock nsnake firefox tree flashplugin-installer && sudo apt-get -f install
 
 # Clean-up
 sudo apt-get autoremove -y
 
 # Copy configs
-sudo cp ~/drat/configs/ratpoisonrc /etc/ratpoisonrc
-sudo cp ~/drat/configs/other/.gitconfig ~/ # My gitconfig
-mkdir .drat
-sudo cp ~/drat/drat-logo.png ~/usr/share/terminology/backgrounds
+sudo cp ~/drat/shared/configs/.ratpoisonrc /etc/ratpoisonrc
+sudo cp ~/drat/shared/configs/other/.gitconfig ~/ # My gitconfig
+
 # start xserver
 startx
